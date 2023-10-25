71215c6797c7b759dc2608adb2341e427959bc06
ages24 <ajegbemiga24@gmail.com>
2023-10-25 13:22:17 +0100

    rendering the image and title dynamically from the data file

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import HeaderImage from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data";
+
+const ReactDescription = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = ReactDescription[genRandomInt(2)];
+
+  return (
+    <header>
+      <img src={HeaderImage} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <ul>
+          <CoreConcept
+            image={CORE_CONCEPTS[0].image}
+            title={CORE_CONCEPTS[0].title}
+            description={CORE_CONCEPTS[0].description}
+          />
+          <CoreConcept
+            image={CORE_CONCEPTS[1].image}
+            title={CORE_CONCEPTS[1].title}
+            description={CORE_CONCEPTS[1].description}
+          />
+          <CoreConcept
+            image={CORE_CONCEPTS[2].image}
+            title={CORE_CONCEPTS[2].title}
+            description={CORE_CONCEPTS[2].description}
+          />
+          <CoreConcept
+            image={CORE_CONCEPTS[3].image}
+            title={CORE_CONCEPTS[3].title}
+            description={CORE_CONCEPTS[3].description}
+          />
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
