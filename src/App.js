1139ee8c855bc6668ae9fcbedb163124aca48e7b
ages24 <ajegbemiga24@gmail.com>
2023-10-25 13:29:08 +0100

    using the spread operator for the core-concepts

--- src/App.js
+++ src/App.js
@@ -39,26 +39,23 @@ function App() {
       <main>
         <section id="core-concepts">
           <ul>
-          <CoreConcept
-            image={CORE_CONCEPTS[0].image}
-            title={CORE_CONCEPTS[0].title}
-            description={CORE_CONCEPTS[0].description}
-          />
-          <CoreConcept
-            image={CORE_CONCEPTS[1].image}
-            title={CORE_CONCEPTS[1].title}
-            description={CORE_CONCEPTS[1].description}
-          />
-          <CoreConcept
-            image={CORE_CONCEPTS[2].image}
-            title={CORE_CONCEPTS[2].title}
-            description={CORE_CONCEPTS[2].description}
-          />
-          <CoreConcept
-            image={CORE_CONCEPTS[3].image}
-            title={CORE_CONCEPTS[3].title}
-            description={CORE_CONCEPTS[3].description}
-          />
+            <CoreConcept
+              image={CORE_CONCEPTS[0].image}
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            {/*  because the name in the core-concepts data file is thesame as the props we are recievingwe can use the spread operator    */}
+            <CoreConcept
+              image={CORE_CONCEPTS[2].image}
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+            />
+            <CoreConcept
+              image={CORE_CONCEPTS[3].image}
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+            />
           </ul>
         </section>
       </main>
